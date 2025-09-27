b84f28a67b1cd8cba9df2945d8f0b69d34378597
Jota Antonio <jamqueiroz2005@gmail.com>
2025-09-27 19:06:55 -0300

    Fix(Book-modal): Corrige erro de transição de modal

--- public/js/ui.js
+++ public/js/ui.js
@@ -153,11 +153,8 @@ export function initializeBookModal() {
     }
 
     const closeModal = () => {
-        modalContent.classList.remove('scale-100');
-        modal.classList.remove('opacity-100');
-        setTimeout(() => {
-            modal.classList.add('hidden');
-        }, 300);
+        modal.classList.remove('modal-active');
+        modal.addEventListener('transitionend', () => modal.classList.add('invisible'), { once: true });
     };
 
     const openModal = (book) => {
@@ -194,10 +191,9 @@ export function initializeBookModal() {
         for (let i = 1; i <= 5; i++) {
             ratingContainer.innerHTML += `<svg class="w-5 h-5 ${i <= book.rating ? 'text-yellow-400' : 'text-gray-300'}" fill="currentColor" viewBox="0 0 20 20"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>`;
         }
-        modal.classList.remove('hidden');
+        modal.classList.remove('invisible');
         setTimeout(() => {
-            modal.classList.add('opacity-100');
-            modalContent.classList.add('scale-100');
+            modal.classList.add('modal-active');
         }, 10);
     };
 
